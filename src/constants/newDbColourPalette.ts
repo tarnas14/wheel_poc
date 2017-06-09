0112a701bc2f7c683bb3ed8af30b667f4482b755
tarnas14 <tarnas14@gmail.com>
2017-06-09 23:25:14 +0200

    adjusted palette ... again

--- src/constants/newDbColourPalette.ts
+++ src/constants/newDbColourPalette.ts
@@ -1,7 +1,7 @@
 export default {
   active: '#0f3a81',
-  pending: '#1981c3',
-  suggestion: '#b2b2b2',
+  pending: '#0098db',
+  suggestion: '#888888',
   icons: 'white',
   raised: '#d05542',
   cta: '#d05542',
